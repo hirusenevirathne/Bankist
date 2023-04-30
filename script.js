ad906c944f9c4ccedaaf883e1f9a3a83cd0b1dce
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-04-30 21:15:29 +0530

    add display movement method

--- script.js
+++ script.js
@@ -61,6 +61,45 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ''; //remove old html line
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `<div class="movements">
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+//<div class="movements__date">3 days ago</div>
+
+displayMovements(account1.movements);
+
+const calDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calDisplayBalance(account1.movements); //hard coded
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -71,4 +110,80 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
+//use array movement method to create a new array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUSD = 1.1;
+
+/*
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUSD;
+}); //funtional method
+*/
+
+//arrow funtion method
+const movementsUSD = movements.map(mov => mov * eurToUSD);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )} }`
+);
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositFor = [];
+for (const mov of movements) if (mov > 0) depositFor.push(mov);
+console.log(depositFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+// the reduce method
+//accumulator -> SNOWBALL
+/*
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Ittration ${i} : ${acc}`);
+  return acc + cur;
+}, 0);
+*/
+
+//arrow funtion method
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+const max = movements.resuce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
+
+/*
+//same answer
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+*/
+
+//console.log(movements);
+//console.log(movementsUSD);
+//console.log(movementsDescriptions);
+
+
+//Chalange 2
+
+
 /////////////////////////////////////////////////
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.maps(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18 );
+  console.log(humanAges);
+  console.log(adults);
+
+  const average = (adults.reduce (acc,age,i,arr) => acc + age / arr.length, 0);
+};
