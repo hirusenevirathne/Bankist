f9e2bb42ee2af21f8d54bcf918ede227403e5541
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-07-10 21:54:15 +0530

    add movements and username to arrays
    create 2 method for show the transactions differently in page and create the username by using first letters of full name and add that username to the account array.

--- script.js
+++ script.js
@@ -61,6 +61,33 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//Display movement method
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ""; //clear the older movements
+  movements.forEach(function (mov, i) {
+
+    const type = mov >0 ? `deposit` : `withdrawal`
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements)
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +101,5 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+
