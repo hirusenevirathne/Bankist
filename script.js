f91214e1267da1c1e2eef71cd46c42d03cf181f4
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-07-24 14:59:14 +0530

    fix number roundings
    add new accounts to check and fix some number rounding with .toFixed(2)

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // BANKIST APP
 
 // Data
+/*
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -34,6 +35,52 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+*/
+
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -75,7 +122,7 @@ const displayMovements = function (movements, sort = false) {
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-          <div class="movements__value">${mov} EUR</div>
+          <div class="movements__value">${mov.toFixed(2)} EUR</div>
         </div>
         `;
 
@@ -88,20 +135,20 @@ const displayMovements = function (movements, sort = false) {
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc,mov)=>acc+mov, 0);//add the balance to the account object
   
-  labelBalance.textContent = `${acc.balance} EUR`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`;
 };
 //calcDisplayBalance(account1.movements); //call the show balance method
 
 const calcDisplaySummery = function (acc) {
   const income = acc.movements.filter(mov => mov>0).reduce((acc,mov ) => acc+mov, 0); //calculate the income
-  labelSumIn.textContent = `${income} EUR`;
+  labelSumIn.textContent = `${income.toFixed(2)} EUR`;
 
   const outcome = acc.movements.filter(mov => mov<0).reduce((acc,mov) => acc+mov, 0); //calculate the out going
-  labelSumOut.textContent = `${Math.abs(outcome)} EUR`
+  labelSumOut.textContent = `${Math.abs(outcome).toFixed(2)} EUR`
 
   const interest = acc.movements.filter(mov => mov>0).map(deposit => (deposit * acc.interestRate)/100).filter((int,i,arr) => 
   {return int >=1;}).reduce((acc,int) => acc+int,0); //calculate the interest and remove all interest less than 1 EUR
-  labelSumInterest.textContent = `${interest} EUR`;
+  labelSumInterest.textContent = `${interest.toFixed(2)} EUR`;
 };
 //calcDisplaySummery(account1.movements); //call the show summery method
 
@@ -178,7 +225,7 @@ btnTransfer.addEventListener('click',function (e) {
 //Request loan method
 btnLoan.addEventListener('click',function (e) {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);//get the loan amount
+  const amount = Math.floor(inputLoanAmount.value);//get the loan amount
   if (amount>0 && currentAccount.movements.some(mov => mov >= amount*0.1)) {//chek if the loan is greater than 10% of any deposit
     //Add movement
     currentAccount.movements.push(amount);
@@ -215,4 +262,3 @@ btnSort.addEventListener('click',function (e) {
   sorted = !sorted;
 });
 
-/////////////////////////////////////////////////
